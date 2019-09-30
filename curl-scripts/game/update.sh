a97830a2de12307188034f090a5e6b7315b60a45
alexgdav <alexgdavidenko@gmail.com>
2019-09-30 12:04:34 -0400

    Continue cleanup Clean-up continued

---
+++ curl-scripts/game/update.sh
@@ -0,0 +1,15 @@
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/:"${ID}"" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "game": {
+      "cell": {
+        "index": "'"{$INDEX}"'"
+        "value": "'{$PLAYER}'"
+      },
+      "over": "'"{$OVER}"'"
+    }
+  }'
